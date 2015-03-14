e125f3a4e0ccbcb4762a5813fafb2f7f589b4064
zahan <this.is.paul.st@gmail.com>
2015-03-14 18:50:05 +0500

    first stable version

--- 20150213_1_weight-rus.py
+++ 20150213_1_weight-rus.py
@@ -1,49 +1,21 @@
 def totalWeight(weight, times): 
         return weight * times 
 
-choice = input('Хотите узнать свой идеальный вес? да/нет\n') 
-
+weight = 24
+choice = input('Ввести подход? да/нет\n') 
+totalCounter = 0
 
 
 while choice == 'да': #Если выбрали да то запускаем калькулятор
-    pol = input('Выберите Ваш пол муж/жен\n')
-    yourWeight = int(input('Введите ваш текущий вес в "кг"\n'))
+    numberOfreps = int(input('Введите количество повторов \n'))
         
-    if pol == 'жен': #Если выбрали женщину то считаем по формуле 1
-            currentHeight = int(input("Введите свой рост в 'см'\n")) 
-            calc = int(idealWeight(currentHeight, pol)) #Отправляем текущий рост и пол в функцию ИдеальныйВес считаем идеальный вес
-            rekomend = yourWeight - calc #Вычисляем рекомендуемый вес вычитаем из того что ввели в начеле идеальный
-            print ("Ваш идеальный вес %d," %calc)
-            if rekomend < 0: 
-                plusRec = abs(rekomend) #Если рекомендация со знаком минус то убираем его с помощью модуля, хотя можно умножить на -1
-                print('Вам нужно набрать %d' %plusRec)
-            else: print('Вам нужно сбросить %d' %rekomend)
-            
-            name = input('Введите Ваше имя\n')
-            print('Всего доброго %s' %name)
-            
-            forLog = open('log.txt', 'a') #Открываем файл лог для добавления строк
-            forLog.write(name + ' - ' + str(yourWeight) + ' - ' + str(calc) + ' - ' + str(rekomend) + '\n' )
-            forLog.close() # Выше формируем нужную запись и закрываем документ тем самым сохраняя его
-            
-            toStart = input('Поробовать еще? да/нет\n') #Спрашиваем хочет ли человек попробовать еще
-            if toStart == 'да':
-                print('Начнем сначала') #Если да то програма просто возращается к началу цикла
-            elif toStart == 'нет':
-                print('Программа завершена') #Если нет то обрываем цикл
-                break
-            
+    calc = int(totalWeight(weight, numberOfreps)) 
+    print ("В этом подходе вы подняли %d, кг." %calc)
     
-    else: print('НЕВЕРНОЕ ЗНАЧЕНИЕ. Введите значение "жен" или "муж"')
-
-if choice == 'нет': #Если пользователь отвечает нет в начале програмы
-    name = input('Введите Ваше имя\n')
-    print('Всего доброго %s' %name)
-    forLog = open('log.txt', 'a')#Открываем файл лог для добавления строк
-    forLog.write(name + ' - Отказался сотрудничать с администрацией\n')
-    forLog.close()# Выше формируем нужную запись и закрываем документ тем самым сохраняя его
-    print('Программа завершена')
-    
+    forLog = open('log.txt', 'a') 
+    forLog.write(str(calc))
+    forLog.close() 
+                
 else:
     print('Неверно!')
     print('Программа завершена!')
