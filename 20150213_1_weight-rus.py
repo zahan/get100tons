f76b1f7d0fbd9559e20949ae930d265f21c5d0a3
zahan <this.is.paul.st@gmail.com>
2015-03-15 13:08:49 +0500

    it's work

--- 20150213_1_weight-rus.py
+++ 20150213_1_weight-rus.py
@@ -4,7 +4,7 @@ totalCounter = 0
 while True:
         choice = input('Ввести подход? y/n\n')
         if choice == 'y':
-               currentReps = int(input('Введите количество повторений'))
+               currentReps = int(input('Введите количество повторений \n'))
                totalCounter += currentReps
 
                currentTotalWeight = currentReps * weight
@@ -12,25 +12,21 @@ while True:
 
                print('В этом подъеме Вы подняли %d кг' %currentTotalWeight)
                print('Всего подянто %d кг' %totalCounterWeight)
-               
-        
 
+               forLog = open('journal.txt', 'a')
+               forLog.write(str(currentTotalWeight) + '\n')
+               forLog.close()
 
+        elif choice == 'n':
+               forLog = open('journal.txt', 'a')
+               forLog.write('total - ' + str(totalCounterWeight) + '\n')
+               forLog.close()
 
-"""
-while choice == 'да': 
-    numberOfreps = int(input('Введите количество повторов \n'))
-        
-    calc = int(totalWeight(weight, numberOfreps)) 
-    print ("В этом подходе вы подняли %d, кг." %calc)
-    
-    forLog = open('log.txt', 'a') 
-    forLog.write(str(calc) + '\n')
-    forLog.close() 
-                
-else:
-    print('Неверно!')
-    print('Программа завершена!')
-"""
+               print('Тренировка закончена. Вы подняли %d кг' %totalCounterWeight)
+               break
+
+        else:
+                print ('Введите корректное значение "у" или "n"')
+
     
 
