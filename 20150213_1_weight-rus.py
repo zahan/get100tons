c11f911bce0c471ae6ceb48622c91ff8d1a8e316
zahan <this.is.paul.st@gmail.com>
2015-03-15 14:08:30 +0500

    Add date for jrn

--- 20150213_1_weight-rus.py
+++ 20150213_1_weight-rus.py
@@ -1,5 +1,14 @@
-weight = 24 
+import datetime
+
+nowDate = datetime.date.today()
+print (nowDate)
+
+
+
+weight = 24 #Вес отягощения
 totalCounter = 0
+
+setCounter = 0
 
 while True:
         choice = input('Ввести подход? y/n\n')
@@ -7,19 +16,21 @@ while True:
                currentReps = int(input('Введите количество повторений \n'))
                totalCounter += currentReps
 
-               currentTotalWeight = currentReps * weight
-               totalCounterWeight = totalCounter * weight
+               currentTotalWeight = currentReps * weight #Вес поднятый в текущем подходе
+               totalCounterWeight = totalCounter * weight #Вес поднятых за все тренировку
+
+               setCounter += int(1) #Записываем номер подхода
 
                print('В этом подъеме Вы подняли %d кг' %currentTotalWeight)
                print('Всего подянто %d кг' %totalCounterWeight)
 
-               forLog = open('journal.txt', 'a')
-               forLog.write(str(currentTotalWeight) + '\n')
+               forLog = open('journal.txt', 'a') #Записываем каждый подход в журнал
+               forLog.write('Подход №' + str(setCounter) + '; ' + 'Вес' + ' - ' + str(currentTotalWeight) + '\n') 
                forLog.close()
 
         elif choice == 'n':
                forLog = open('journal.txt', 'a')
-               forLog.write('total - ' + str(totalCounterWeight) + '\n')
+               forLog.write('=' * 21 + '\n' + ' - Суммарный вес - ' + str(totalCounterWeight) + '\n' + '=' * 21 + '\n') #Записываем в журнал суммарный вес за треню
                forLog.close()
 
                print('Тренировка закончена. Вы подняли %d кг' %totalCounterWeight)
